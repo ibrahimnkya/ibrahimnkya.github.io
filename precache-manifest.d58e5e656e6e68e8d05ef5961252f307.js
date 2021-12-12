1993215a9569c3830c1542f82222e558d513e8a7
Ibrahim Nkya <ibra.isn@gmail.com>
2021-12-12 20:02:26 +0300

    Updates

---
+++ precache-manifest.d58e5e656e6e68e8d05ef5961252f307.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "99e7d930764f172fd06b",
+    "url": "/portfolio/static/css/main.b561f4ca.chunk.css"
+  },
+  {
+    "revision": "99e7d930764f172fd06b",
+    "url": "/portfolio/static/js/main.acfed019.chunk.js"
+  },
+  {
+    "revision": "f24dfa47ccca6ab4ec98",
+    "url": "/portfolio/static/js/runtime~main.b2c72c8f.js"
+  },
+  {
+    "revision": "9ef4a78cb61b2aacca01",
+    "url": "/portfolio/static/css/2.3105772d.chunk.css"
+  },
+  {
+    "revision": "9ef4a78cb61b2aacca01",
+    "url": "/portfolio/static/js/2.b1686414.chunk.js"
+  },
+  {
+    "revision": "8508d8c2220a76bf9cf3",
+    "url": "/portfolio/static/js/3.a0b9342c.chunk.js"
+  },
+  {
+    "revision": "f2e7f9affc4b0789f77de8f844e1d36c",
+    "url": "/portfolio/static/media/pre.f2e7f9af.svg"
+  },
+  {
+    "revision": "db364d1c06f6169a5d5ae45982f34531",
+    "url": "/portfolio/static/media/uiux.db364d1c.png"
+  },
+  {
+    "revision": "21db42bfa9bd655af79acf3d35782440",
+    "url": "/portfolio/static/media/IbrahimNkya-Resume.21db42bf.pdf"
+  },
+  {
+    "revision": "db6659c6edbffc16fb2169604f2d5af4",
+    "url": "/portfolio/static/media/logo.db6659c6.png"
+  },
+  {
+    "revision": "c93911478ff7341f3b985130e12278fc",
+    "url": "/portfolio/static/media/me.c9391147.jpg"
+  },
+  {
+    "revision": "a56b61d3cfa62897605ff9da8d627022",
+    "url": "/portfolio/index.html"
+  }
+];
